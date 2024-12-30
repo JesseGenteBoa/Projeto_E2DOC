28b8758a51b63b99cbf142a789765261eef3b838
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-12-30 19:27:03 -0300

    Update conexaoDB.py

--- conexaoDB.py
+++ conexaoDB.py
@@ -14,16 +14,24 @@ def consultar_db(cpf):
 
         cursor = conn.cursor()
 
-        sql = f"SELECT * FROM import_csv WHERE cpf = {cpf};"
-        cursor.execute(sql)
-        consulta = cursor.fetchone()
+        try:
+            sql = f"SELECT * FROM import_csv WHERE cpf = {cpf};"
+            cursor.execute(sql)
+            consulta = cursor.fetchone()
 
-        regiao = consulta['regiao_pcs']
-        centro_de_custo = consulta['cc']
-        nome = consulta['funcionario']
+            regiao = consulta['regiao_pcs']
+            centro_de_custo = consulta['cc']
+            nome = consulta['funcionario']
 
-        conn.close()
-        return regiao, centro_de_custo, nome
+            conn.close()
+            return regiao, centro_de_custo, nome
+        
+        except Exception:
+            regiao = False
+            centro_de_custo = False
+            nome = False
+            return regiao, centro_de_custo, nome
     
     except Exception as e:
         return Exception(f"Erro ao se conectar com o banco de dados: {e}")
+
